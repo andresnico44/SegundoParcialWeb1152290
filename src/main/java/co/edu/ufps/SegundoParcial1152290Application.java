b8dc1367a197d5b484798e94cddb91f12bde9991
andresnico44 <ramiroandresnc@ufps.edu.co>
2024-11-08 08:28:44 -0500

    Base Segundo Parcial

---
+++ src/main/java/co/edu/ufps/SegundoParcial1152290Application.java
@@ -0,0 +1,13 @@
+package co.edu.ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoParcial1152290Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoParcial1152290Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ufps/SegundoParcial1152290ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoParcial1152290ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
